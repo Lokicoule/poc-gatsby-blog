85294c52c464d02479498dfe6d28759d0bc36dbb
lokicoule <lokicoule@yahoo.fr>
2022-04-09 00:15:26 +0200

    Finished Gatsby Tutorial Part 7

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -15,6 +15,7 @@ const config: GatsbyConfig = {
       },
     },
     "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
   ],
 };
 
---
+++ src/pages/blog/{mdx.slug}.tsx
@@ -0,0 +1,40 @@
+import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+import * as React from "react";
+import Layout from "../../components/layout";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+
+const BlogPost = ({ data }: any) => {
+  const image = getImage(data.mdx.frontmatter.hero_image);
+  return (
+    <Layout pageTitle={data.mdx.frontmatter.title}>
+      <p>Posted: {data.mdx.frontmatter.date}</p>
+      {image && (
+        <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
+      )}
+      <MDXRenderer>{data.mdx.body}</MDXRenderer>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: { eq: $id }) {
+      body
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default BlogPost;
