d5107f55734aceeec82bdf051033f3e36479b96c
lokicoule <lokicoule@yahoo.fr>
2022-04-08 23:25:43 +0200

    Finished Gatsby Tutorial Part 4

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,10 +1,21 @@
 import type { GatsbyConfig } from "gatsby";
-
+import path from "path";
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `new`,
+    title: `poc blog`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: path.resolve(`blog`),
+      },
+    },
+    "gatsby-plugin-mdx",
+  ],
 };
 
 export default config;
---
+++ src/pages/blog.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
+
+type BlogPageProps = {
+  data: {
+    allFile: {
+      nodes: [{ name: string }];
+    };
+  };
+};
+
+const BlogPage = ({ data }: BlogPageProps) => {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      <ul>
+        {data.allFile.nodes.map((node) => (
+          <li key={node.name}>{node.name}</li>
+        ))}
+      </ul>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
+
+export default BlogPage;
